085da13a71ae917a134ce0cb85cd813512d2afbf
BernieYe <bony960323@gmail.com>
2021-11-17 15:30:56 -0500

    Mark_Policy

--- mark_data.py
+++ mark_data.py
@@ -9,6 +9,8 @@ from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium import webdriver
 import csv
 import pickle
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
 
 class Param:
     def __init__(self):
@@ -62,14 +64,18 @@ class SeleniumWeb:
     def get_cust_number_xpath(self):
         return '//form[@id="AccountInfo"]//div[contains(@class, "SectionItem")]/div[1]/div//div[@class="value"]'
 
-    def get_button_edit_xpath(self):
-        return '//form[@id="AccountInfo"]/div[contains(@class, "SectionButtons")]/a[contains(@class, "section_edit")]'
+    def get_button_edit_xpath(self, is_package):
+        if is_package:
+            return '//form[@id="PackageAdditionalInfo"]/div[contains(@class, "SectionButtons")]/a[contains(@class, "section_edit")]'
+        return '//form[@name="PolicyAdditional"]/div[contains(@class, "SectionButtons")]/a[contains(@class, "section_edit")]'
 
-    def get_button_save_xpath(self):
-        return '//form[@id="AccountInfo"]/div[contains(@class, "SectionButtons")]/a[contains(@class, "section_save")]'
+    def get_button_save_xpath(self, is_package):
+        if is_package:
+            return '//form[@name="PackageAdditionalInfo"]/div[contains(@class, "SectionButtons")]/a[contains(@class, "section_save")]'
+        return '//form[@name="PolicyAdditional"]/div[contains(@class, "SectionButtons")]/a[contains(@class, "section_save")]'
 
     def get_original_agent_xpath(self):
-        return '//form[@id="AccountInfo"]//div[contains(@class, "SectionItem")]//label[@for="account-info-agent"]/following-sibling::div[@class="value"]'
+        return '//form[@name="PolicyAdditional"]//label[contains(text(), "Agent")]/following-sibling::div[@class="value"]'
 
     def get_original_csr_xpath(self):
         return '//form[@id="AccountInfo"]//div[contains(@class, "SectionItem")]//label[@for="account-info-csr"]/following-sibling::div[@class="value"]'
@@ -78,7 +84,21 @@ class SeleniumWeb:
         return '/html/body//div[@class="search-results"]/div[contains(@class, "no-search-results")]'
 
     def get_policies_from_list_xpath(self):
-        return '//form[@id="PolicyList"]/table[@class="subgrid"]/tbody/tr[contains(@class, "policyStatusEquals")]'
+        return '//form[@id="PolicyList"]/table[@class="subgrid"]/tbody/tr[contains(@class, "policyStatusEquals")]/td[contains(@class, "lob")]/div[2]'
+
+    def get_policy_premium_xpath(self):
+        return '//section[@id="overview-panel"]//div[contains(@class, "policypremium")]'
+
+    def get_policy_number_xpath(self):
+        return '//section[@id="overview-panel"]//div[contains(@class, "policyno")]'
+
+    def get_policy_status_xpath(self, is_package = False):
+        if is_package:
+            return '//form[@id="PackagePolicyInfo"]//div[contains(@class, "PolicyInfo-PolicyStatus")]/div[contains(@class, "statusText")]'
+        return '//form[@name="PolicyInfo"]//div[contains(@class, "PolicyInfo-PolicyStatus")]/div[contains(@class, "statusText")]'
+
+    def get_policy_agent_input_xpath(self):
+        return '//select[@name="AgentID"]/option'
 
     def login(self, login_email, login_password):
         WebDriverWait(self.driver, 60).until(lambda d: d.find_element_by_xpath('//input[@type="password"]'))
@@ -139,12 +159,28 @@ class SeleniumWeb:
         button_policy_tab.click()
         return self.driver.current_url
 
-    def access_policy_detail(self):
+    def access_policies(self, policy_tab_url):
         policy_items_xpath = self.get_policies_from_list_xpath()
         policy_elements = self.driver.find_elements_by_xpath(policy_items_xpath)
         if policy_elements:
-            for policy_element in policy_elements:
-                pass
+            return policy_elements
+        else:
+            print('No policy with this customer')
+            return
+
+    def access_single_policy_details(self, policy_element):
+        self.scroll(policy_element)
+        policy_element.click()
+
+        time.sleep(3)
+        is_package = self.check_exists_by_id('PackagePolicyInfo')
+
+        policy_number = self.driver.find_element_by_xpath(self.get_policy_number_xpath()).text
+        policy_premium = self.driver.find_element_by_xpath(self.get_policy_premium_xpath()).text
+        policy_status = self.driver.find_element_by_xpath(self.get_policy_status_xpath(is_package)).text
+        policy_ori_agent = self.get_original_agent()
+
+        return policy_number, policy_premium, policy_status, policy_ori_agent, is_package
 
     def check_customer_numer(self, cust_number_in_dataset):
         cust_number_xpath = self.get_cust_number_xpath()
@@ -155,6 +191,7 @@ class SeleniumWeb:
         original_agent_xpath = self.get_original_agent_xpath()
         if self.check_exists_by_xpath(original_agent_xpath):
             ori_agent = self.driver.find_element_by_xpath(original_agent_xpath).text
+            print('Original Agent: ' + ori_agent)
             return ori_agent
         print('No Original Agent Found')
         return ''
@@ -167,17 +204,27 @@ class SeleniumWeb:
         print('No Original CSR Found')
         return ''
 
-    def start_to_edit(self):
-        button_edit_xpath = self.get_button_edit_xpath()
+    def start_to_edit(self, is_package = False):
+        button_edit_xpath = self.get_button_edit_xpath(is_package)
         if self.check_exists_by_xpath(button_edit_xpath):
             button_edit = self.driver.find_element_by_xpath(button_edit_xpath)
-            button_edit.click()
+            self.scroll(button_edit)
             time.sleep(2)
+            if self.check_clickable_by_xpath(button_edit_xpath):
+                #button_edit.click()
+                time.sleep(2)
+                return True
+            else:
+                print('Edit Button Not Clickable')
         else:
             print('No Edit Button Found')
 
+        return False
+
     def mark_agent(self, value="12816"):
-        dropdown_agent = Select(self.driver.find_element_by_id('account-info-agent'))
+        agent_id_element = self.driver.find_element_by_name('AgentID')
+        self.scroll(agent_id_element)
+        dropdown_agent = Select(agent_id_element)
         dropdown_agent.select_by_value(value)
         time.sleep(5)
 
@@ -186,11 +233,14 @@ class SeleniumWeb:
         dropdown_csr.select_by_value(value)
         time.sleep(5)
 
-    def save_info(self):
-        button_save_xpath = self.get_button_save_xpath()
+    def save_info(self, is_package = False):
+        button_save_xpath = self.get_button_save_xpath(is_package)
         if self.check_exists_by_xpath(button_save_xpath):
             button_save = self.driver.find_element_by_xpath(button_save_xpath)
-            button_save.click()
+            self.scroll(button_save)
+            if not self.check_clickable_by_xpath(button_save_xpath):
+                print('Save Button Not Clickable')
+        time.sleep(5)
 
 
 def read_csv_data(csv_path):
@@ -229,27 +279,44 @@ def main():
     print('Get {number} pieces of data'.format(number=str(len(customer_rows))))
 
     '''
+    Open Record Files
+    '''
+    pp_policy_num_f = open(param.processed_policy_number_path, 'ab')
+    pp_policy_records_f = open(param.processed_policy_records_path, 'a')
+    '''
     Mark Account Info As Transfer Account
     '''
-
-    processed_cust_num_file = open('processed_cust_num_set.txt', 'rb')
-    objects = []
+    # Customer Number
+    processed_cust_num_file = open(param.processed_customer_number_path, 'rb')
+    cust_num_list = []
     while True:
         try:
-            objects.append(pickle.load(processed_cust_num_file))
+            cust_num_list.append(pickle.load(processed_cust_num_file))
         except EOFError:
             break
     processed_cust_num_file.close()
-    processed_cust_num_set = set(objects[1:])
-
+    processed_cust_num_set = set(cust_num_list[1:])
+    print('Processed Customer: {number} '.format(number=str(len(processed_cust_num_set))))
+    # Policy Number
+    processed_policy_num_file = open(param.processed_customer_number_path, 'rb')
+    policy_num_list = []
+    # while True:
+    #     try:
+    #         policy_num_list.append(pickle.load(processed_policy_num_file))
+    #     except EOFError:
+    #         break
+    # processed_policy_num_file.close()
+    processed_policy_num_set = set(policy_num_list[1:])
+    print('Processed Policy: {number} '.format(number=str(len(processed_policy_num_set))))
     # with open('processed_cust_num_set.txt', 'rb') as f:
     #     processed_cust_num_set = set(pickle.load(f))
     # print(processed_cust_num_set)
     for index, cust in enumerate(customer_rows):
-        cust_full_name = cust['Customer First Name'] + ' ' + cust['Customer Last Name']
-
+        cust_first_name, cust_last_name = cust['Customer First Name'], cust['Customer Last Name']
         cust_num = cust['Customer Number']
-        print(str(index) + ' ' + cust_full_name + ' ' + cust_num)
+        cust_full_name = cust_first_name + ' ' + cust_last_name
+
+        print(f'--- No.{index} {cust_full_name}  {cust_num}')
         if cust_num in processed_cust_num_set:
             print('exist!')
             continue
@@ -263,7 +330,7 @@ def main():
             processed_cust_num_set.add(cust_num)
             driver.get(param.qq_index)
             time.sleep(5)
-            with open('processed_cust_num_set.txt', 'ab') as f:
+            with open(processed_cust_num_file, 'ab') as f:
                 pickle.dump(cust_num, f)
             continue
 
@@ -275,6 +342,49 @@ def main():
                 break
             # Go to the policy tab
             policy_tab_url = selenium_web.access_policy_tab()
+            time.sleep(5)
+            policy_number = len(selenium_web.access_policies(policy_tab_url))
+            if policy_number:
+                for index in range(policy_number):
+                    policy_items = selenium_web.driver.find_elements_by_xpath(selenium_web.get_policies_from_list_xpath())
+                    policy = policy_items[index]
+                    selenium_web.scroll(policy)
+                    plc_num_from_preview = policy.text
+                    print('  {index}. {plc_num_from_preview}'.format(index=index, plc_num_from_preview=plc_num_from_preview))
+                    # if plc_num_from_preview in processed_policy_num_set:
+                    #     continue
+
+                    plc_num, premium, status, ori_agent, is_package= selenium_web.access_single_policy_details(policy)
+                    if not is_package:
+                        if_clickable = selenium_web.start_to_edit()
+                        if if_clickable :
+                            selenium_web.mark_agent()
+                            selenium_web.save_info()
+                    else:
+                        if_clickable = selenium_web.start_to_edit(is_package)
+                        if if_clickable:
+                            selenium_web.mark_agent()
+                            selenium_web.save_info(is_package)
+
+                    # record the policy info
+                    policy_record = {'Customer First Name': cust_first_name, 'Customer Last Name': cust_last_name, \
+                                     'Customer Number': cust_num, 'Policy Num': plc_num, \
+                                     'Policy Premium': premium, 'Policy Status': status, \
+                                     'Agent On Policy': ori_agent}
+
+                    processed_policy_num_set.add(plc_num)
+
+                    with open(param.processed_policy_number_path, 'ab') as pp_policy_num_f:
+                        pickle.dump(plc_num, pp_policy_num_f)
+
+                    with open(param.processed_policy_records_path, 'a') as pp_policy_records_f:
+                        policy_record_writer = csv.DictWriter(pp_policy_records_f, fieldnames = policy_record.keys())
+                        policy_record_writer.writerow(policy_record)
+
+                    selenium_web.driver.get(policy_tab_url)
+                    time.sleep(5)
+
+
 
             # # Get the Original Agent & CSR
             # original_agent = selenium_web.get_original_agent()
@@ -289,10 +399,10 @@ def main():
 
         processed_cust_num_set.add(cust_num)
 
-        with open('processed_cust_num_set.txt', 'ab') as f:
+        with open(param.processed_customer_number_path, 'ab') as f:
             pickle.dump(cust_num, f)
 
-        with open('tmp_2020_customer_processed.csv', 'a', newline='') as csvfile:
+        with open(param.processed_customer_records_path, 'a', newline='') as csvfile:
             fieldnames = list(cust.keys())
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
